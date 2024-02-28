546ffe3ca7db705564859158ad4f8c70736fd7ad
Talha Malik <talhamalik25.tm@gmail.com>
2024-02-28 20:29:11 +0500

    Added bootstrap

---
+++ Bootstrap_Lecture/Lecture 2/app.js
@@ -0,0 +1,6 @@
+const navbar = document.querySelector('.navbar');
+const html_element = document.querySelector('html');
+
+console.dir(html_element)
+
+html_element.setAttribute("data-bs-theme", "dark")
