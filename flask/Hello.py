fe677a24bc7010ff489b0de4dc83d2c703946201
Talha Malik <talhamalik25.tm@gmail.com>
2024-03-13 03:48:18 -0700

    flask app

---
+++ flask/Hello.py
@@ -0,0 +1,14 @@
+from flask import Flask , render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return render_template('index.html')
+   # return 'Hello World'
+
+@app.route('/products')
+def products():
+   return 'this is a product page'
+
+if __name__ == "__main__":
+   app.run(debug=True, port=8000)
